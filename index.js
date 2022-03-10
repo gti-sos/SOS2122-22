2fa0972f5d171d314494e617f34b84e7493d7a7a
belrodsal <belrodsal@alum.us.es>
2022-03-10 18:54:40 +0100

    añado ruta dinámica cool

---
+++ index.js
@@ -0,0 +1,19 @@
+const cool = require("cool-ascii-faces");
+
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/cool",(req,res)=>{
+    console.log("Requested / route");
+    res.send("<html><body>"+cool()+"</body></html>")
+});
+
+app.use("/index", express.static('public'));
+
+
+
+app.listen(port, () =>{
+    console.log(`server TRULY ready at the port ${port}`);
+});
