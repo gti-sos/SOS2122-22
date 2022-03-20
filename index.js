0a69099fa9053fc6298908737d56ef62a33d7719
jesvencam <jesusvenac17@gmail.com>
2022-03-20 08:59:02 +0100

    Añadido el código del L05 para el acceso a la api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cool = require("cool-ascii-faces");
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 app.get("/cool",(req,res)=>{
     console.log("Requested / route");
@@ -17,3 +17,92 @@ app.use("/", express.static('public'));
 app.listen(port, () =>{
     console.log(`server TRULY ready at the port ${port}`);
 });
+
+
+
+/********   API Jesús Vena Campos  *********/
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+//const app = express();
+//const port = process.env.PORT || 8081;
+
+app.use(bodyParser.json());
+
+const BASE_API_URL = "/api/v1";
+
+var contacts = [
+    {		
+        country : "España",
+        year : 2020,
+        co2_tot : 214.817,
+        co2_kg : 0.13,
+        co2_tpc : 4.62
+    },
+    {
+        country : "Alemania",
+        year : 2017,
+        co2_tot : 250.177,
+        co2_kg : 0.15,
+        co2_tpc : 4.77
+    }
+];
+
+app.get(BASE_API_URL+"/contacts",(req,res)=>{
+    res.send(JSON.stringify(contacts,null,2));
+
+});
+
+app.get(BASE_API_URL+"/contacts",(req,res)=>{
+    res.redirect(API_DOC_PORTAL);
+
+});
+
+
+app.post(BASE_API_URL+"/contacts",(req,res)=>{
+    contacts.push(req.body);
+    res.sendStatus(201,"CREATED");
+
+});
+//ESTO BORRA TODOS LOS RECURSO  
+app.delete(BASE_API_URL+"/contacts", (req, res)=>{
+    contacts = [];
+    res.sendStatus(200,"OK");
+});
+
+
+app.delete(BASE_API_URL+"/contacts/:name", (req, res)=>{
+    var contactName = req.params.name;
+    contacts.filter((contact)=>{
+        return(contact.name!=contactName);
+    })
+    res.sendStatus(200,"OK");
+});
+
+
+app.get(BASE_API_URL+"/contacts/:name", (req, res)=>{
+    var contactName = req.params.name;
+    filteredContacts = contacts.filter((contact)=>{
+        return(contact.name == contactName);
+    })
+    if(filteredContacts == 0){
+        res.sendStatus(404,"NOT FOUND");
+    }else{
+        res.send(JSON.stringify(filteredContacts[0],null,2));
+    }
+
+    res.sendStatus(200,"OK");
+
+
+});
+
+
+
+app.use("/",express.static('public'));
+
+//para que esté escuchando en un puerto 
+app.listen(port,() =>{
+console.log(`Server ready at port ${port}`)
+});
+
