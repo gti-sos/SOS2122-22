70fa783694aaa86ebdd028a70ef1b27ce0a39f46
jesvencam <jesusvenac17@gmail.com>
2022-03-25 15:29:47 +0100

    Añadidas las tareas para el FO6

--- index.js
+++ index.js
@@ -64,16 +64,17 @@ app.post(BASE_API_URL+"/trade-stats",(req,res)=>{
 //######################   API Jesús Vena Campos  ###############################//
 
 
+
 var co2 = [
     {		
-        country : "España",
+        country : "spain",
         year : 2020,
         co2_tot : 214.817,
         co2_kg : 0.13,
         co2_tpc : 4.62
     },
     {
-        country : "Alemania",
+        country : "alemania",
         year : 2017,
         co2_tot : 250.177,
         co2_kg : 0.15,
@@ -81,37 +82,64 @@ var co2 = [
     }
 ];
 
+//##########################################
+//GET
 app.get(BASE_API_URL+"/co2-stats",(req,res)=>{
+    if(co2.length==0){
+        co2.push({		
+            country : "mexico",
+            year : 2020,
+            co2_tot : 214.817,
+            co2_kg : 0.13,
+            co2_tpc : 4.62
+        },
+        {
+            country : "italia",
+            year : 2017,
+            co2_tot : 250.177,
+            co2_kg : 0.15,
+            co2_tpc : 4.77
+        },{		
+            country : "francia",
+            year : 2020,
+            co2_tot : 214.817,
+            co2_kg : 0.13,
+            co2_tpc : 4.62
+        },
+        {
+            country : "rusia",
+            year : 2017,
+            co2_tot : 250.177,
+            co2_kg : 0.15,
+            co2_tpc : 4.77
+        },{		
+            country : "spain",
+            year : 2020,
+            co2_tot : 214.817,
+            co2_kg : 0.13,
+            co2_tpc : 4.62
+        },
+        {
+            country : "alemania",
+            year : 2017,
+            co2_tot : 250.177,
+            co2_kg : 0.15,
+            co2_tpc : 4.77
+        })
+
+    }
     res.send(JSON.stringify(co2,null,2));
 
 });
+app.get(BASE_API_URL+"/co2-stats/docs",(req,res)=>{
+    res.redirect("https://documenter.getpostman.com/view/19481636/UVsTphXD");
 
-app.get(BASE_API_URL+"/co2-stats",(req,res)=>{
+});
+
+app.get(BASE_API_URL+"/co2-stats/docs",(req,res)=>{
     res.redirect(API_DOC_PORTAL);
 
 });
-
-
-app.post(BASE_API_URL+"/co2-stats",(req,res)=>{
-    co2.push(req.body);
-    res.sendStatus(201,"CREATED");
-
-});
-//ESTO BORRA TODOS LOS RECURSO  
-app.delete(BASE_API_URL+"/co2-stats", (req, res)=>{
-    co2 = [];
-    res.sendStatus(200,"OK");
-});
-
-
-app.delete(BASE_API_URL+"/co2-stats/:country", (req, res)=>{
-    var countryName = req.params.country;
-    co2.filter((c)=>{
-        return(c.country!=countryName);
-    })
-    res.sendStatus(200,"OK");
-});
-
 
 app.get(BASE_API_URL+"/co2-stats/:country", (req, res)=>{
     var countryName = req.params.country;
@@ -123,10 +151,83 @@ app.get(BASE_API_URL+"/co2-stats/:country", (req, res)=>{
     }else{
         res.send(JSON.stringify(filteredCountries[0],null,2));
     }
+
     res.sendStatus(200,"OK");
 
 
 });
+
+//##########################################
+
+//POST
+
+
+app.post(BASE_API_URL+"/co2-stats",(req,res)=>{
+    co2.push(req.body);
+    res.sendStatus(201,"CREATED");
+
+});
+
+app.post(BASE_API_URL+"/co2-stats/:country",(req,res)=>{
+    var countryName = req.params.country;
+    filteredCountries = co2.filter((c)=>{
+        return(c.country == countryName);
+    });
+    if(filteredCountries == 0){
+        res.sendStatus(409,"Conflict");
+    }else{
+        res.sendStatus(405,"Method Not Allowed");
+    }
+  
+
+});
+
+//##########################################
+
+//PUT
+
+
+app.put(BASE_API_URL+"/co2-stats",(req,res)=>{
+    res.sendStatus(405,"Method Not Allowed");
+
+})
+
+app.put(BASE_API_URL+"/co2-stats/:country/:year",(req,res)=>{
+    var countryName = req.params.country;
+    var yearReq = req.params.year;
+    filteredCountryYear = co2.filter((c)=>{
+        return(c.country == countryName && c.year == yearReq);
+    });
+    if(filteredCountryYear == 0){
+        res.sendStatus(400,"Bad Request");
+    }else{
+        co2.push(req.body)  ;
+        //co2.splice(filteredCountryYear);
+
+        res.sendStatus(200,"OK");
+    }
+})
+
+
+//##########################################
+//DELETE
+
+
+app.delete(BASE_API_URL+"/co2-stats/:country", (req, res)=>{
+    var countryName = req.params.country;
+    co2.filter((c)=>{
+        return(c.country!=countryName);
+        co2.pop()
+    })
+    res.sendStatus(200,"OK");
+});
+
+//ESTO BORRA TODOS LOS RECURSO  
+app.delete(BASE_API_URL+"/co2-stats", (req, res)=>{
+    co2 = [];
+    res.sendStatus(200,"OK");
+});
+
 
 //##########################################
 
