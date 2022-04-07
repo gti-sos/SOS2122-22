105b8dbdbfa13a8d49b2bef862180678d78100dd
jesvencam <jesusvenac17@gmail.com>
2022-04-07 10:34:18 +0200

    mEJORA DEL PROYECTO AÑADIENDODB

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
-
+const bodyParser = require("body-parser")
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT || 8080;
 
 
@@ -10,14 +9,18 @@ const coal_stats_API = require("./src/back/belrodsalAPI/indexBelrodsal.js");
 const co2_stats_API = require("./src/back/jesvencamAPI/indexJESVENCAM.js");
 const trade_stats_API = require("./src/back/marsaamar1API/index_marsaamar1.js");
 
+const Datastore = require('nedb');
+
+db_co2_stats = new Datastore();
+
+co2_stats_API.register(app,db_co2_stats);
+
 const BASE_API_URL = "/api/v1";
 
-app.use(bodyParser.json());
 
 app.use("/",express.static('public'));
 
 coal_stats_API(app);
-co2_stats_API.register(app);
 trade_stats_API(app);
 
 //----------------------------------------------------
