3811781a69705d1e4e52a9397b144a3514e6a599
belrodsal <belrodsal@alum.us.es>
2022-04-25 12:55:26 +0200

    Pequeño cambio

--- src/front/info.svelte
+++ src/front/info.svelte
@@ -10,7 +10,7 @@
     <p><a href="/#/tradeStatsTable">Table of Trade Stats</a></p>
     <h3>TEAM</h3>
     <p>- <a href="https://github.com/jesvencam">Jesús Vena Campos</a></p>
-    <p>- <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a></p>
+    <p>- <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a> ,Fuente de Datos : Coal-Stats</p>
     <p>- <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a></p>
     <h3>Project description:</h3> Nuesto proyecto consta en analizar la relación existente entre las emisiones de CO2, la produccion de carbón y la balanza comercial entre diferentes paises en relacion a los ultimos años. 
     <h3>Repository:</h3><a href="https://github.com/gti-sos/SOS2122-22.git">gti-sos/SOS2122-22</a>
@@ -19,7 +19,7 @@
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/co2-stats/docs">https://sos2122-22.herokuapp.com/api/v1/co2-stats/docs </a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/#/Co2StatsTable">Co2StatsTable jesvencam's frontEnd</a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
 
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v1/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
+    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/#/CoalStatsTable">CoalStatsTable belrodsal's frontEnd </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
 
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</p>
