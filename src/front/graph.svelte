90cf20980f30b41c44ab324624fad6e0851abf4e
jesvencam <jesusvenac17@gmail.com>
2022-04-28 15:16:19 +0200

    Añadida la gráfica basica

---
+++ src/front/graph.svelte
@@ -0,0 +1,107 @@
+<script>
+
+    import {onMount} from'svelte';
+
+    let apiData = [];
+
+    async function getData(){
+        const res = await fetch("/api/v2/co2-stats/data");
+        if (res.ok){
+            const json = await res.json();
+            console.log("lsdknfipndv"+JSON.stringify(json));
+            apiData = json;
+            console.log("cargando el grafo con los datos nuevos"+apiData);
+            loadGraph();
+        }else{
+            console.log("Error in request");
+        }
+    }
+
+    async function loadGraph(){
+
+        Highcharts.chart('container', {
+
+            title: {
+                text: 'Solar Employment Growth by Sector, 2010-2016'
+            },
+
+            subtitle: {
+                text: 'Source: thesolarfoundation.com'
+            },
+
+            yAxis: {
+                title: {
+                    text: 'Year'
+                }
+            },
+
+            xAxis: {
+                accessibility: {
+                    rangeDescription: 'Range: 2010 to 2017'
+                }
+            },
+
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            plotOptions: {
+                series: {
+                    label: {
+                        connectorAllowed: false
+                    },
+                    pointStart: 2010
+                }
+            },
+
+            series: [{
+                name: 'Installation',
+                data: apiData
+                }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
+                        }
+                    }
+                }]
+            }
+
+        });
+
+
+    }
+    onMount(getData);
+   
+</script>
+<main>
+
+    <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p class="highcharts-description">
+            Basic line chart showing trends in a dataset. This chart includes the
+            <code>series-label</code> module, which adds a label to each line for
+            enhanced readability.
+        </p>
+    </figure>
+
+</main>
+
+<svelte:head>
+
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/series-label.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+
+</svelte:head>
