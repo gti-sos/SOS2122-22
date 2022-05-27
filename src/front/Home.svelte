cd1068cfff311c231a3e309537a4a1599c0b27bb
belrodsal <belrodsal@alum.us.es>
2022-05-27 12:17:36 +0200

    Cambios estéticos

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -1,38 +1,55 @@
+<main>
+    <div class="container">
+        <div id="titulo">
+            <h1>SOS2122-22</h1>
+        </div>
+        <h2><a href="/#/info">Página de Info </a></h2> 
 
-<script>
-
-</script>
-<main> 
-    <body>
-    <h1>SOS2122-22</h1>	
-    <p><a href="/#/info">INFO</a></p>
-    <p><a href="/#/Co2StatsTable">Table of CO2 Stats</a></p>
-    <p><a href="/jesvencam">Video de Jesús Vena.</a></p>
-
-    <p><a href="/#/coalStatsTable">Table of Coal Stats</a></p>
-    <p><a href="/#/tradeStatsTable">Table of Trade Stats</a></p>
-   
-
-    <p><a href="/#/graph">GRAFICA GRUPAL </a></p>
-
-
-    <h3>TEAM</h3>
-    <p>- <a href="/jesvencam">Jesús Vena Campos</a></p>
-                    
-    <p>- <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a></p>
-    <p>- <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a></p>
-    <h3>Project description:</h3> Nuesto proyecto consta en analizar la relación existente entre las emisiones de CO2, la produccion de carbón y la balanza comercial entre diferentes paises en relacion a los ultimos años. 
-    <h3>Repository:</h3><a href="https://github.com/gti-sos/SOS2122-22.git">gti-sos/SOS2122-22</a>
-    <h3>URL:</h3><a href="http://sos2122-22.herokuapp.com">http://sos2122-22.herokuapp.com</a>
-    <h3>APIS:</h3>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats/"> CO2-STATS API V2</a></p>
-    <p>- <a href="https://documenter.getpostman.com/view/19481636/UVyrVwjh">Documentacion de la V1 de la api de co2 </a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
+        <h2>Team</h2>
+        <p>- <a href="/jesvencam">Jesús Vena Campos</a></p>
+        <p>- <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a></p>
+        <p>- <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a></p>
+        <h3>Project description:</h3> Nuesto proyecto consta en analizar la relación existente entre las emisiones de CO2, la produccion de carbón y la balanza comercial entre diferentes paises en relacion a los ultimos años. 
+        <h3>Repository:</h3><a href="https://github.com/gti-sos/SOS2122-22.git">gti-sos/SOS2122-22</a>
+        <h3>URL:</h3><a href="http://sos2122-22.herokuapp.com">http://sos2122-22.herokuapp.com</a>
+        <br>
+        <h2>Videos</h2>
+        <p><a href="/jesvencam">Video de Jesús Vena.</a></p>
+    
+        <hr>
+        <h4>DOCS</h4>
+        <p>- <a href="https://documenter.getpostman.com/view/19481636/UVyrVwjh">Documentacion de la V1 de la api de co2 </a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
 
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats/docs">Documentacion de la API V2 de co2</a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v1/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v2/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</p>
-</body>
-</main>
+ 
+        <h4>APIS</h4>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v1/co2-stats">https://sos2122-23.herokuapp.com/api/v1/co2-stats </a> </li>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats">https://sos2122-23.herokuapp.com/api/v2/co2-stats </a> </li>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats">https://sos2122-22.herokuapp.com/api/v1/coal-stats </a> </li>  
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats">https://sos2122-22.herokuapp.com/api/v2/coal-stats </a> </li> 
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/trade-stats">https://sos2122-22.herokuapp.com/api/v2/trade-stats </a> </li>
+        <br>
+        <h4>FRONT END</h4>
+        <li><a href="https://sos2122-22.herokuapp.com/#/Co2StatsTable">Co2StatsTable jesvencam's frontEnd</a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</li>
+        <li><a href="https://sos2122-22.herokuapp.com/#/CoalStatsTable">CoalStatsTable belrodsal's frontEnd </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</li>
+        <li><a href="https://sos2122-22.herokuapp.com/#/tradeStatsTable">TradeStatsTable marsaamar1's frontEnd </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martínez</a>)</li>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</li>
+        <hr>
 
+        
+    </div>
+        
+</main>
+<style>
+    .container {
+        padding: 2px 16px;
+        
+    }
+    #titulo{
+        text-align: center;
+    }
+</style>
--- src/front/info.svelte
+++ src/front/info.svelte
@@ -1,47 +1,73 @@
+<main>
+    <div class="container">
+        <div id="titulo" class ="row">
+            <h1>SOS2122-22 INFO</h1>
+            <p></p>
+            <div class="col-4">
+            
+            <h6>Jesus Vena</h6>
+            <p><a href="/#/Co2StatsTable">Table of CO2 Stats</a></p>
+            <p><a href="/">Página de Jesús Vena Campos</a></p>
+            <p><a href="/jesvencam">Video de Jesús Vena.</a></p>
+            </div>
+            <div class="col-4">
+                <h6>Belén Rodriguez</h6>
+                <p><a href="/#/coalStatsTable">Table of Coal Stats</a></p>
+            </div>
+            <div class="col-4">
+                <p><a href="/#/tradeStatsTable">Table of Trade Stats</a></p>
+            </div>
+        </div>
 
-<script>
+        <h2>Team</h2>
 
-</script>
-<main> 
-    <body>
-    <h1>SOS2122-22</h1>	
-    <p><a href="/#/Co2StatsTable">Table of CO2 Stats</a></p>
-    <p><a href="/">Página de Jesús Vena Campos</a></p>
-    <p><a href="/#/coalStatsTable">Table of Coal Stats</a></p>
-    <p><a href="/#/tradeStatsTable">Table of Trade Stats</a></p>
-
+        <li><a href="/#/graph">GRAFICA GRUPAL1</a></li>
+        <li><a href="/#/analytics">GRAFICA GRUPAL2</a></li>
     
-    <h3>TEAM</h3>
-    <a href="/#/graph">GRAFICA GRUPAL</a>
+        <li><a href="/#/integrations">Integraciones</a></li>
+        <br>
+        <p>- <a href="/jesvencam">Jesús Vena Campos</a></p>
+                    
+        <p>- <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a></p>
+        <p>- <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a></p>
+        <h3>Project description:</h3> Nuesto proyecto consta en analizar la relación existente entre las emisiones de CO2, la produccion de carbón y la balanza comercial entre diferentes paises en relacion a los ultimos años. 
+        <h3>Repository:</h3><a href="https://github.com/gti-sos/SOS2122-22.git">gti-sos/SOS2122-22</a>
+        <h3>URL:</h3><a href="http://sos2122-22.herokuapp.com">http://sos2122-22.herokuapp.com</a>
     
-    <p><a href="/#/integrations">Integraciones</a></p>
-    
-    <p></p>
-    <p>- <a href="https://github.com/jesvencam">Jesús Vena Campos</a></p>
-    <p>- <a href="https://github.com/beldruxs">Belén Rodríguez Salazar </a> (Fuende de Datos: Coal-Stats)</p>
-    <p>- <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a></p>
-    <h3>Project description:</h3> Nuesto proyecto consta en analizar la relación existente entre las emisiones de CO2, la produccion de carbón y la balanza comercial entre diferentes paises en relacion a los ultimos años. 
-    <h3>Repository:</h3><a href="https://github.com/gti-sos/SOS2122-22.git">gti-sos/SOS2122-22</a>
-    <h3>URL:</h3><a href="http://sos2122-22.herokuapp.com">http://sos2122-22.herokuapp.com</a>
-    <h3>APIS:</h3>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/co2-stats/"> CO2-STATS API V1</a></p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats/"> CO2-STATS API V2</a></p>
+        <hr>
+        <h4>DOCS</h4>
+        <p>- <a href="https://documenter.getpostman.com/view/19481636/UVyrVwjh">Documentacion de la V1 de la api de co2 </a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
 
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats/"> COAL-STATS API</a></p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/coal-stats/"> COAL-STATS API V1</a></p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/trade-stats/"> TRADE-STATS API</a></p>
-    <h3>DOCUMENTACION:</h3>
-    <p>- <a href="https://documenter.getpostman.com/view/19481636/UVyrVwjh">https://sos2122-22.herokuapp.com/api/v1/co2-stats/docs </a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats/docs">https://sos2122-22.herokuapp.com/api/v2/co2-stats/docs </a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
+    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats/docs">Documentacion de la API V2 de co2</a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v1/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</p>
+    <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs">https://sos2122-22.herokuapp.com/api/v2/coal-stats/docs </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v2/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v2/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</p>
-    <h3>FRONT-ENDS:</h3>
-    <p>- <a href="https://sos2122-22.herokuapp.com/#/Co2StatsTable">Co2StatsTable jesvencam's frontEnd</a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/#/CoalStatsTable">CoalStatsTable belrodsal's frontEnd </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</p>
-    <p>- <a href="https://sos2122-22.herokuapp.com/#/tradeStatsTable">TradeStatsTable marsaamar1's frontEnd </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martínez</a>)</p>
     <p>- <a href="https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</p>
-</body>
-</main>
+ 
+        <h4>APIS</h4>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v1/co2-stats">https://sos2122-23.herokuapp.com/api/v1/co2-stats </a> </li>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/co2-stats">https://sos2122-23.herokuapp.com/api/v2/co2-stats </a> </li>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats">https://sos2122-22.herokuapp.com/api/v1/coal-stats </a> </li>  
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/coal-stats">https://sos2122-22.herokuapp.com/api/v2/coal-stats </a> </li> 
+        <li><a href="https://sos2122-22.herokuapp.com/api/v2/trade-stats">https://sos2122-22.herokuapp.com/api/v2/trade-stats </a> </li>
+        <br>
+        <h4>FRONT END</h4>
+        <li><a href="https://sos2122-22.herokuapp.com/#/Co2StatsTable">Co2StatsTable jesvencam's frontEnd</a>(developed by <a href="https://github.com/jesvencam">Jesús Vena Campos</a>)</li>
+        <li><a href="https://sos2122-22.herokuapp.com/#/CoalStatsTable">CoalStatsTable belrodsal's frontEnd </a>(developed by <a href="https://github.com/beldruxs">Belén Rodríguez Salazar</a>)</li>
+        <li><a href="https://sos2122-22.herokuapp.com/#/tradeStatsTable">TradeStatsTable marsaamar1's frontEnd </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martínez</a>)</li>
+        <li><a href="https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs">https://sos2122-22.herokuapp.com/api/v1/trade-stats/docs </a>(developed by <a href="https://github.com/MarcoSaa01">Marcos Saa Martinez</a>)</li>
+        <hr>
 
+        
+    </div>
+        
+</main>
+<style>
+    .container {
+        padding: 2px 16px;
+        
+    }
+    #titulo{
+        text-align: center;
+    }
+</style>
